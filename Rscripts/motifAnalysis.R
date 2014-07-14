8969a79c00ae2686f34f75c38c43a1b3c9cf3e1f
jjborrelli <borrejj@gmail.com>
2014-07-14 10:46:05 +0200

    update motif profiles plots and code

--- Rscripts/motifAnalysis.R
+++ Rscripts/motifAnalysis.R
@@ -30,9 +30,9 @@ motif_counter(testA2, webs = names(mot.lst))
 ## Functions to get eigenvalues of randomly sampled matrices
 ran.unif <- function(motmat){
   newmat <- apply(motmat, c(1,2), function(x){
-    if(x==1){runif(1, 0, 5)}else if(x==-1){runif(1, -1, 0)} else{0}
+    if(x==1){runif(1, 0, 10)}else if(x==-1){runif(1, -1, 0)} else{0}
   })
-  diag(newmat) <- -1
+  diag(newmat) <- runif(3, -1, 0)
   return(newmat)
 }
 
@@ -69,7 +69,7 @@ eig.analysis <- function(n, matrices, mode = "unif"){
 }
 
 ## Run with uniform
-n <- 5000
+n <- 10000
 system.time(
   mot.stab<- eig.analysis(n, mot.lst, mode = "unif")
 )
@@ -96,29 +96,38 @@ ggplot(m.l, aes(x = Var2, y = value)) + geom_boxplot() + geom_hline(aes(yinterce
 mot.qss <- apply(mot.stab, 2, function(x){sum(x<0)/n})
 sorted <- sort(mot.qss, decreasing = T)
 sort.qss <- data.frame(sorted, names = names(sorted))
+
 #Lnorm
 mot.qss.l <- apply(mot.stab.l, 2, function(x){sum(x<0)/n})
 sorted.l <- sort(mot.qss.l, decreasing = T)
 sort.qss.l <- data.frame(sorted.l, names = names(sorted))
 
 # Read in and reshape subgraph frequency data
-motcount <- read.csv("C:/Users/borre_000/Desktop/GitHub/Ecological-Networks/FoodWebs/Tables/zscore_both.csv", row.names = 1)
+motcount <- read.csv("C:/Users/borre_000/Desktop/GitHub/Ecological-Networks/FoodWebs/Tables/zscore_both2.csv", row.names = 1)
 df.freq <- data.frame(motcount)
 
-mfreq <- melt(df.freq[,names(sorted)])
+mfreq <- melt(sub.counts[,ord])
+ord <- c("s5", "s4", "s1", "s2", "d3", "d1", "d4", "d2", "d8", "d5", "s3", "d7", "d6")
 
 g <- ggplot(mfreq, aes(x = variable, y = log(value)/max(log(value)))) + geom_boxplot() 
-g <- g + geom_line(data = sort.qss, aes(x = 1:13, y = sorted), size = 1.5)
+#g <- g + geom_line(data = sort.qss, aes(x = 1:13, y = sorted), size = 1.5)
 g <- g + geom_point(data = sort.qss, aes(x = 1:13, y = sorted), size = 4, col = "blue")
-g <- g + geom_line(data = sort.qss.l, aes(x = 1:13, y = sorted.l), size = 1.5)
-g <- g + geom_point(data = sort.qss.l, aes(x = 1:13, y = sorted.l), size = 4, col = "darkred")
+#g <- g + geom_line(data = sort.qss.l, aes(x = 1:13, y = sorted.l), size = 1.5)
+#g <- g + geom_point(data = sort.qss.l, aes(x = 1:13, y = sorted.l), size = 4, col = "darkred")
 g <- g + geom_hline(aes(yintercept = 0), lty = 2, col = "red")
-g + xlab("Subgraph") + ylab(expression(over("Log Frequency", "Max(Log Frequency)")))
+g + xlab("Subgraph") + ylab(expression(over("Log(Frequency)","Max(Log(Frequency))")))
 
+cor.test(apply(z.both[,names(sorted)], 2, function(x){mean(x)}), sorted)
 
-temp.df <- data.frame(matrix(rep(mot.qss, 50), ncol = 13, byrow = T))
+temp.df <- data.frame(matrix(rep(mot.qss.l, 50), ncol = 13, byrow = T))
 colnames(temp.df) <- names(mot.lst)
 mstab <- melt(temp.df)
+
+dat1 <- data.frame(sub = names(sorted), qss = sorted, freq = apply(sub.counts[,names(sorted)], 2, mean))
+fit <- glm(round(cbind(10000*dat1$qss, 10000-10000*dat1$qss))~dat1$freq, family = "binomial")
+plot(dat1$freq, dat1$qss)
+points(fit$fitted.values~dat1$freq, pch = 20, col = "red")
+
 
 #######
 
@@ -158,28 +167,28 @@ clusterExport(cl = clus, list = c("rbindlist", "permutes_prob", "motif_counter")
 registerDoSNOW(cl)
 
 system.time(
-pmat <- parLapply(cl, web.matrices, function(x){rbindlist(permutes_prob(x, iter = 1000, FUN = motif_counter, webs = 1))})
+pmat <- parLapply(cl, web.matrices, function(x){rbindlist(permutes_prob(x, iter = 200, FUN = motif_counter, webs = 1))})
 )
 #45 min for 1000 iterations
 
 stopCluster(clus)
 
-nullmean <- t(sapply(pmat, function(x){apply(x, 2, mean)}))[,2:14]
-nullsd <- t(sapply(pmat, function(x){apply(x, 2, sd)}))[,2:14]
-nullmot <- (sub.counts - nullmean)/nullsd
-nullmot[is.na(nullmot)] <- 0
+nullmean2 <- t(sapply(pmat, function(x){apply(x, 2, mean)}))[,2:14]
+nullsd2 <- t(sapply(pmat, function(x){apply(x, 2, sd)}))[,2:14]
+nullmot2 <- (sub.counts - nullmean2)/nullsd2
+nullmot2[is.na(nullmot2)] <- 0
 
-nullmot <- apply(nullmot, 2, function(x){x/sqrt(sum(x^2))})
+nullmot2a <- t(apply(nullmot2, 1, function(x){x/sqrt(sum(x^2))}))
 
-boxplot(nullmot[,names(sorted)])
+boxplot(nullmot2a[,names(sorted)])
 
-mfreq.rcp <- melt(nullmot[,names(sorted)])
+mfreq.rcp <- melt(nullmot2a[,names(sorted)])
 
 p1 <- ggplot(mfreq.rcp, aes(x = Var2, y = value)) + geom_boxplot() 
-p1 <- p1 + geom_line(data = sort.qss, aes(x = 1:13, y = sorted), size = 1.5)
-p1 <- p1 + geom_point(data = sort.qss, aes(x = 1:13, y = sorted), size = 4, col = "blue")
-p1 <- p1 + geom_line(data = sort.qss.l, aes(x = 1:13, y = sorted.l), size = 1.5)
-p1 <- p1 + geom_point(data = sort.qss.l, aes(x = 1:13, y = sorted.l), size = 4, col = "darkred")
+#p1 <- p1 + geom_line(data = sort.qss, aes(x = 1:13, y = sorted), size = 1.5)
+#p1 <- p1 + geom_point(data = sort.qss, aes(x = 1:13, y = sorted), size = 4, col = "blue")
+#p1 <- p1 + geom_line(data = sort.qss.l, aes(x = 1:13, y = sorted.l), size = 1.5)
+#p1 <- p1 + geom_point(data = sort.qss.l, aes(x = 1:13, y = sorted.l), size = 4, col = "darkred")
 p1 <- p1 + geom_hline(aes(yintercept = 0), lty = 2, col = "red")
 p1 + xlab("Subgraph") + ylab("Frequency")
 
@@ -214,6 +223,77 @@ m <- apply(mcEIG, 2, min)
 
 summary(betareg(qss~motN2))
 
+
+
+###### MY NULL MODEL ROWS AND COLUMN SUMS 
+
+permutes_rc <- function(mat, iter = 100){
+  
+  pattern1 <- matrix(c(0,1,1,0), nrow = 2, ncol = 2)
+  pattern2 <- matrix(c(1,0,0,1), nrow = 2, ncol = 2)
+  count <- 0
+  
+  mat.list <- list()
+  
+  while(count < iter){
+    srow <- sample(1:nrow(mat), 2)
+    scol <- sample(1:ncol(mat), 2)
+    
+    test <- mat[srow, scol]
+    
+    if(sum(test == pattern1) == 4){
+      count <- count + 1
+      mat[srow, scol] <- pattern2
+      mat.list[[count]] <- mat
+      
+      next
+    } else if(sum(test == pattern2) == 4){
+      count <- count + 1
+      mat[srow, scol] <- pattern1
+      mat.list[[count]] <- mat
+      
+      next
+    } else {next}
+  }
+  
+  matrices <- lapply(mat.list, as.matrix)
+  return(permuted.matrices = matrices)
+}
+
+require(igraph)
+pmot <- list()
+
+system.time(
+for(i in 1:length(web.matrices)){
+  p <- permutes_rc(web.matrices[[i]], 1000)
+  g <- lapply(p, graph.adjacency)
+  pmot[[i]] <- motif_counter(g, 1:1000)
+  cat(i, names(web.matrices)[i], "\n")
+}
+)
+
+mus <- t(sapply(pmot, function(x){colMeans(x[,2:14])}))
+sig <- t(sapply(pmot, function(x){apply(x[,2:14], 2, sd)}))
+z <- (sub.counts - mus)/sig
+zmat <- as.matrix(z)
+zmat[is.nan(zmat)] <- 0
+profile <- apply(zmat, 2, function(x){x/sqrt(rowSums(zmat^2))})
+
+require(ggplot2)
+require(reshape2)
+
+norm.pro <- melt(profile[,names(sorted)])
+
+p1 <- ggplot(norm.pro, aes(x = Var2, y = value)) + geom_boxplot() 
+p1 <- p1 + geom_line(data = sort.qss, aes(x = 1:13, y = sorted), size = 1.5)
+p1 <- p1 + geom_point(data = sort.qss, aes(x = 1:13, y = sorted), size = 4, col = "blue")
+p1 <- p1 + geom_line(data = sort.qss.l, aes(x = 1:13, y = sorted.l), size = 1.5)
+p1 <- p1 + geom_point(data = sort.qss.l, aes(x = 1:13, y = sorted.l), size = 4, col = "darkred")
+p1 <- p1 + geom_hline(aes(yintercept = 0), lty = 2, col = "red")
+p1 + xlab("Subgraph") + ylab("Normalized Profile")
+
+cor.test(sorted, apply(profile[,names(sorted)], 2, median))
+
 setwd("C:/Users/borre_000/Desktop/GitHub/Subgraph-Stability/")
-#save.image("subgraphSTABILITY.Rdata")
+#save.image("subgraphSTABILITY2.Rdata")
 load("subgraphSTABILITY.Rdata")
